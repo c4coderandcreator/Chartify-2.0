4039b5fe468cc90336eea01b4e68156c67aa2573
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2024-03-24 10:43:47 +0530

    server fix

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
 const ResetPassword = () => {
