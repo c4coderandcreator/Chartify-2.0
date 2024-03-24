39a91ebdf65e8008e935cf82346170d4565f3be3
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2024-03-24 10:17:29 +0530

    server fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://chartify-2-0.onrender.com"],
     credentials: true,
   })
 );
